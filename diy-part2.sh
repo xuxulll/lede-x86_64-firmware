9b3ad96a5772634fad1a950e499a4bb1a72095a4
Ray <4958511+xuxulll@users.noreply.github.com>
2022-01-22 00:37:34 +0800

    Update diy-part2.sh
    change open clash version to v0.42.10-beta
    remove other gfw tool

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,9 +14,9 @@
 sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
 
 # Clone extra packages
-#git clone --recursive https://github.com/vernesong/OpenClash package/openclash
-git clone --recursive https://github.com/kenzok8/openwrt-packages package/kenzok
-git clone --recursive https://github.com/kenzok8/small package/small
+git clone -b v0.42.10-beta --recursive https://github.com/vernesong/OpenClash package/openclash
+#git clone --recursive https://github.com/kenzok8/openwrt-packages package/kenzok
+#git clone --recursive https://github.com/kenzok8/small package/small
 git clone --recursive https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 # Replace default Argon Theme
