6f15ca54a4ef137926b46a942d6c7d5306d03aad
Ray <xuxulll@users.noreply.github.com>
2023-08-12 00:25:27 +0800

    Update openclash version

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,10 +15,10 @@ sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_genera
 
 # Clone extra packages
 # git clone -b v0.42.10-beta --recursive https://github.com/vernesong/OpenClash package/openclash
-curl -L https://github.com/vernesong/OpenClash/archive/refs/tags/v0.42.10-beta.tar.gz > openclash.tar.gz
+curl -L https://github.com/vernesong/OpenClash/archive/refs/tags/v0.45.129-beta.tar.gz > openclash.tar.gz
 tar -zxvf openclash.tar.gz
 mkdir package/openclash
-cp -R OpenClash-0.42.10-beta/luci-app-openclash package/openclash/luci-app-openclash
+cp -R OpenClash-0.45.129-beta/luci-app-openclash package/openclash/luci-app-openclash
 #git clone --recursive https://github.com/kenzok8/openwrt-packages package/kenzok
 #git clone --recursive https://github.com/kenzok8/small package/small
 git clone --recursive https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
